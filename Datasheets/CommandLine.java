bce10080c5d818f15ca96d0bfbf20ca21f02e208
Amogh Sunil Gajare <amoghsgajare@gmail.com>
2019-05-16 22:21:34 +0530

    Add files via upload

---
+++ Datasheets/CommandLine.java
@@ -0,0 +1,26 @@
+import java.io.*; 
+
+public class CommandLine 
+{ 
+public static void main(String args[]) 
+{ 
+try 
+{
+String com = "start text.txt"; 
+Process p=Runtime.getRuntime().exec("cmd /c "+com); 
+p.waitFor(); 
+BufferedReader reader=new BufferedReader(new InputStreamReader(p.getInputStream())); 
+String line=reader.readLine(); 
+while(line!=null) 
+{ 
+System.out.println(line); 
+line=reader.readLine(); 
+} 
+
+} 
+catch(IOException e1) {} 
+catch(InterruptedException e2) {} 
+
+System.out.println("Done"); 
+} 
+} 
